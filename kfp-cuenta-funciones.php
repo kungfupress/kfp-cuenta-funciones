2199a4155a8429427238fa5d0c730d8f95d57986
Juan Antonio Ruiz Rivas <juanan@us.es>
2019-06-25 21:30:40 +0200

    Cabecera del plugin y esqueleto función de activación

---
+++ kfp-cuenta-funciones.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: KFP Cuenta Funciones
+ * Author: Alumnos KungFuPress
+ * Version: 0.1
+ * Description: Calcula las estadísticas de aparición de funciones de los plugins instalados en este WordPress
+ **/
+ 
+ register_activation_hook(__FILE__, 'Kfp_Cuenta_funciones_activation');
+ 
+ function Kfp_Cuenta_funciones_activation()
+ {
+ 
+ }
